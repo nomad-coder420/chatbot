ed115ac6dfbfc12625f15496895444aa3acf2c1c
nomad-coder420 <saketmahajan0@gmail.com>
2025-01-25 07:58:35 +0530

    webpack publicPath fix

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,6 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"), // Output directory
     filename: "bundle.js", // Output file name
-    publicPath: "/public",
   },
   module: {
     rules: [
