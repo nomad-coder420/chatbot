a8dd47b9f000c011794593db1fd3108b408ee2f3
nomad-coder420 <saketmahajan0@gmail.com>
2025-01-25 23:35:07 +0530

    access token in local storage

--- src/redux/thunks/auth.ts
+++ src/redux/thunks/auth.ts
@@ -1,12 +1,25 @@
 import { callLoginApi } from "../../service/auth";
 
+export const setAccessToken = (token: string) => {
+  localStorage.setItem("accessToken", token);
+};
+
+export const getAccessToken = () => {
+  return localStorage.getItem("accessToken");
+};
+
 export const loginUser = async (authCode: string) => {
   try {
     const response = await callLoginApi(authCode);
 
-    console.log(response);
+    const accessToken = response.access_token;
 
-    return true;
+    if (accessToken) {
+      setAccessToken(accessToken);
+      return true;
+    }
+
+    return false;
   } catch (err) {
     console.log(err);
     alert("Something went wrong, please try again later");
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,12 +3,20 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import LoginScreen from "../view/screens/login/";
 import ChatScreen from "../view/screens/chat/";
+import { getAccessToken } from "../redux/thunks/auth";
 
 const AppRouter = () => {
+  const access = getAccessToken();
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to={"/login"} />} />
+        <Route
+          path="/"
+          element={
+            access ? <Navigate to={"/chat"} /> : <Navigate to={"/login"} />
+          }
+        />
         <Route path="/login" element={<LoginScreen />} />
         <Route path="/chat/" element={<ChatScreen />} />
       </Routes>
